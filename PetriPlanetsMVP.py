538a8adae5385068f9e0f9862d89f975fe5bc2ac
DaanBio <daan@vandervorm.com>
2022-10-30 10:48:27 +0100

    First commit

---
+++ PetriPlanetsMVP.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import pandas as pd
+import time
+import random 
+
+df=pd.read_excel("database2.xlsx")
+
+images=st.empty()
+
+
+#st.title("PetriPlanets MVP")
+col1, col2 = st.columns(2)
+
+with col1:
+
+    if st.button('Travel to the next planet'):
+        with images.container():
+            randnum=random.randint(0, (df.shape[0]-1))
+            url1=df["url"][randnum]
+            st.markdown(f"![Foo]({url1})")
+            st.title(df['Name'][randnum])
+            st.markdown(df['stories'][randnum])
+    
+    else:
+        pass
+
+
+
+#hide_streamlit_style = """
+#            <style>
+#            #MainMenu {visibility: hidden;}
+#            footer {visibility: hidden;}
+#            </style>
+#            """
+#st.markdown(hide_streamlit_style, unsafe_allow_html=True)

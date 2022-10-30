a4efae6605536d0fef068204c2a0a9b50122694c
DaanBio <daan@vandervorm.com>
2022-10-30 14:37:53 +0100

    First commit

---
+++ 1_introduction.py
@@ -0,0 +1,57 @@
+import streamlit as st
+from streamlit.components.v1 import html
+
+def nav_page(page_name, timeout_secs=3):
+    nav_script = """
+        <script type="text/javascript">
+            function attempt_nav_page(page_name, start_time, timeout_secs) {
+                var links = window.parent.document.getElementsByTagName("a");
+                for (var i = 0; i < links.length; i++) {
+                    if (links[i].href.toLowerCase().endsWith("/" + page_name.toLowerCase())) {
+                        links[i].click();
+                        return;
+                    }
+                }
+                var elasped = new Date() - start_time;
+                if (elasped < timeout_secs * 1000) {
+                    setTimeout(attempt_nav_page, 100, page_name, start_time, timeout_secs);
+                } else {
+                    alert("Unable to navigate to page '" + page_name + "' after " + timeout_secs + " second(s).");
+                }
+            }
+            window.addEventListener("load", function() {
+                attempt_nav_page("%s", new Date(), %d);
+            });
+        </script>
+    """ % (page_name, timeout_secs)
+    html(nav_script)
+
+st.set_page_config(
+    page_title="Bio Tools",
+    page_icon=":dna:",
+    initial_sidebar_state="collapsed"
+)
+
+
+
+st.markdown("<style> ul {display: none;} </style>", unsafe_allow_html=True)
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+
+
+
+
+with open('introduction.txt', 'r') as file:
+    data = file.read()
+
+st.title("Welcome to the PetriPlanet Universe")
+st.markdown(data )
+if st.button("Start your journey"):
+    nav_page("PetriPlanetsMVP")
+
--- pages/2_PetriPlanetsMVP.py
+++ pages/2_PetriPlanetsMVP.py
@@ -3,7 +3,9 @@ import pandas as pd
 import time
 import random 
 
+
 df=pd.read_excel("database2.xlsx")
+
 
 images=st.empty()
 
@@ -13,13 +15,15 @@ col1, col2 = st.columns(2)
 
 with col1:
 
-    if st.button('Travel to the next planet'):
+    if st.button("Travel to the next planet"):
         with images.container():
             randnum=random.randint(0, (df.shape[0]-1))
             url1=df["url"][randnum]
             st.markdown(f"![Foo]({url1})")
             st.title(df['Name'][randnum])
             st.markdown(df['stories'][randnum])
+            
+
     
     else:
         pass
@@ -33,3 +37,4 @@ hide_streamlit_style = """
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+st.markdown("<style> ul {display: none;} </style>", unsafe_allow_html=True)
